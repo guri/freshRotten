20ff0e1638bdb069b5f82f8ef13f965bb0bdbf8d
guri <makes.reality@gmail.com>
2014-11-29 12:42:53 +0200

    playing with adding ionic infinite scroll, but after changes browsersync breaks.

--- client/scripts/freshrotten/controllers/searchRottenCtrl.js
+++ client/scripts/freshrotten/controllers/searchRottenCtrl.js
@@ -13,43 +13,57 @@ module.exports = function(app) {
         var vm = this;
         vm.message = 'Something is rotten here !';
 
-        // vm.movies = [
-        //     {
-        //         "title":"bla will return",
-        //         "synopsis":"bla bla movie bla",
-        //         "posters.thumbnail":"1.jpg"
-        //     }, 
-        //    {    
-        //         "title":"Bam man",
-        //         "synopsis":"bam bam dam dam do",
-        //         "posters.thumbnail":"2.jpg"
-        //     },
-        //     {
-        //         "title":"Bla in the forest",
-        //         "synopsis":"bla bla movie bla",
-        //         "posters.thumbnail":"1.jpg"
-        //     }, 
-        //    {
-        //         "title":"Bam man 2",
-        //         "synopsis":"bam bam dam dam do",
-        //         "posters.thumbnail":"2.jpg"
-        //     }                        
-        // ];
+        /* vm.movies = [
+            {
+                "title":"bla will return",
+                "synopsis":"bla bla movie bla",
+                "posters.thumbnail":"1.jpg"
+            }, 
+           {    
+                "title":"Bam man",
+                "synopsis":"bam bam dam dam do",
+                "posters.thumbnail":"2.jpg"
+            },
+            {
+                "title":"Bla in the forest",
+                "synopsis":"bla bla movie bla",
+                "posters.thumbnail":"1.jpg"
+            }, 
+           {
+                "title":"Bam man 2",
+                "synopsis":"bam bam dam dam do",
+                "posters.thumbnail":"2.jpg"
+            }                           //movies mock
+        ]; */
 
         //console.log("3+4=" + searchRotten.add(3,4));
         //console.log("http : " + searchRotten.searchMovies("a"));
 
- 
         
-        vm.searchUpdated = function() {
-            console.log(vm.query);
-            var res = searchRotten.searchMovies(vm.query);
+        vm.searchUpdated = function(query) {  // call for search in rotten api
+            console.log(query);
+            var res = searchRotten.searchMovies(query);
             res.then(function(data) {
                 //console.log(data);
                 vm.movies = data.movies;
             });
 
         };
+
+
+        // support loading more movies for inifinite scroll vs. pagination.
+        vm.loadMore = function(vm.query) {
+            searchRotten.searchMovies(vm.query).then(function(data) {
+              //useItems(items);
+              vm.movies = data.movies;
+              //vm.$broadcast('scroll.infiniteScrollComplete');
+            });
+        };
+
+        // vm.$on('$stateChangeSuccess', function() {
+        //     vm.loadMore();
+        // });
+
         //vm.movies = res[0];
 
 
